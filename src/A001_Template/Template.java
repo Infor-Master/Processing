8164b96690077f7e97c6c681f51aaaba6d3da1c4
Rodrigo Soares <rodrigo.escola@gmail.com>
2020-07-09 21:33:11 +0100

    A001_fix

--- src/A001_Template/Template.java
+++ src/A001_Template/Template.java
@@ -1,23 +1,36 @@
 package A001_Template;
 
-import processing.core.PApplet;
+import processing.core.*;
 
 public class Template extends PApplet {
 
+    // TODO: Customize screen size and so on here
+    @Override
     public void settings(){
         size(500, 500);
     }
 
+    // TODO: Your custom drawing and setup on applet start belongs here
+    @Override
+    public void setup(){
+        this.frameRate(60);
+        this.noStroke();
+    }
+
+    // TODO: Do your drawing for each frame here
+    @Override
     public void draw(){
         ellipse(mouseX, mouseY, 50, 50);
     }
 
+    @Override
     public void mousePressed(){
-        background(64);
+        //clear();
+        //background(64);
     }
 
     public static void main(String[] args){
-        String[] processingArgs = {"Template"};
+        String[] processingArgs = {"A001_Template.Template"};
         Template currentSketch = new Template();
         PApplet.runSketch(processingArgs, currentSketch);
     }
